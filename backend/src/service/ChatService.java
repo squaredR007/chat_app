d37808e19bfe6a64939c47a634fdbcf9a92db8c1
Zeinab-Daneshyar <zei6nab@gmail.com>
2026-06-17 04:04:42 +0330

    save message method changed to static

--- backend/src/service/ChatService.java
+++ backend/src/service/ChatService.java
@@ -5,7 +5,7 @@ import repository.ChatRepository ;
 import java.util.List ;
 
 public class ChatService {
-    private ChatRepository chatRepository ;
+    private static ChatRepository chatRepository ;
 
     public ChatService(ChatRepository chatRepository) {
         this.chatRepository = chatRepository ;
@@ -13,14 +13,14 @@ public class ChatService {
 
     //Creating a private chat between 2 users and save it
 
-    public PrivateChat creatPrivateChat (String chatId , String user1 , String user2) {
+    public static PrivateChat creatPrivateChat (String chatId , String user1 , String user2) {
         PrivateChat chat = new PrivateChat(chatId , user1 , user2) ;
         chatRepository.save(chat);
         return chat ;
     }
 
     //Creating saved messages
-    public PrivateChat createSavedMessagesChat(String username) {
+    public static PrivateChat createSavedMessagesChat(String username) {
         String savedChatId = "saved_" + username;
         return creatPrivateChat(savedChatId, username, username);
     }
